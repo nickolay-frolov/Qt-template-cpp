ada5a4d4b6f21bc9c80dd35da5c7fdcee052666d
LORD-dev86 <lord.dev86@gmail.com>
2024-03-02 19:08:10 +0500

    Upload project files

---
+++ include/MainWindow.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <QtWidgets//QMainWindow>
+#include <QMouseEvent>
+#include "ui_MainWindow.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget* parent = nullptr);
+    void setupStyle();
+
+    void on_btn_close_clicked();
+    void on_btn_minimize_clicked();
+
+    ~MainWindow();
+
+private:
+    Ui::MainWindowClass ui;
+
+protected:
+    QPoint m_DragPosition;
+
+    void mousePressEvent(QMouseEvent* event) override
+    {
+        m_DragPosition = event->globalPos() - this->frameGeometry().topLeft();
+        event->accept();
+    }
+
+    void mouseMoveEvent(QMouseEvent* event) override
+    {
+        if (event->buttons() & Qt::LeftButton) {
+            this->move(event->globalPos() - m_DragPosition);
+            event->accept();
+        }
+    }
+};
---
+++ src/MainWindow.cpp
@@ -0,0 +1,49 @@
+#include <QFile>
+#include <QDebug>
+
+#include "..\include\MainWindow.h"
+#include <QPushButton>
+
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
+{
+	ui.setupUi(this);
+	this->setupStyle();
+	this->setWindowFlags(Qt::FramelessWindowHint);
+
+	QPushButton* btn_close = findChild<QPushButton*>("btn_close");
+	QPushButton* btn_minimize = findChild<QPushButton*>("btn_minimize");
+
+	connect(btn_close, &QPushButton::clicked, this, &MainWindow::on_btn_close_clicked);
+	connect(btn_minimize, &QPushButton::clicked, this, &MainWindow::on_btn_minimize_clicked);
+}
+
+void MainWindow::setupStyle()
+{
+	QString styleFilePath = "MainStyle.qss";
+
+	try {
+		QFile styleFile(styleFilePath);
+		styleFile.open(QFile::ReadOnly);
+		QString style = styleFile.readAll();
+		qApp->setStyleSheet(style);
+		styleFile.close();
+	}
+	catch (std::exception& ex) {
+
+	}
+}
+
+void MainWindow::on_btn_close_clicked()
+{
+	this->close();
+}
+
+void MainWindow::on_btn_minimize_clicked()
+{
+	this->showMinimized();
+}
+
+MainWindow::~MainWindow()
+{
+
+}
